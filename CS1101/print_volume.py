8aeba69ffe0ba8928eb26076bbe449de19f4cde6
Omi Akif <45337017+omi-akif@users.noreply.github.com>
2021-02-07 00:31:26 +0600

    Learning Journal Unit 2 Part 1 complete

---
+++ CS1101/print_volume.py
@@ -0,0 +1,13 @@
+import math as m
+
+
+def print_volume(radius): 
+
+    volume = 4 / 3 * m.pi * radius ** 3
+
+    print(volume)
+
+
+print_volume(5) #Assuming a radius of 5 units
+print_volume(6) #Assuming a radius of 6 units
+print_volume(8) #Assuming a radius of 7 units
